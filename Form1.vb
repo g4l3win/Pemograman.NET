4ce361b52feaf95ac1e4a17d862c140178d611e5
Acer <Acer@LAPTOP-FTNPN94M>
2023-09-06 18:05:23 +0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = TextBox1.Text
+        TextBox1.BackColor = Color.Aqua
+        Button2.BackColor = Color.Aquamarine
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
